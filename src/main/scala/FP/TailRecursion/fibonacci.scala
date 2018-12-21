0d5d573cadd5bd438e789d9424beac1857286bde
Jeff Gaoey <gaoey@hotmail.com>
2018-12-21 15:52:35 +0700

    fix syntax for fibonacci

--- src/main/scala/FP/TailRecursion/fibonacci.scala
+++ src/main/scala/FP/TailRecursion/fibonacci.scala
@@ -5,8 +5,11 @@ object fibonancial {
   def fib(n: Int): Int = {
     @annotation.tailrec
     def loop(n: Int, prev: Int, cur: Int): Int =
-      if (n <= 0) prev
-      else loop(n - 1, cur, prev + cur)
+      if (n <= 0)
+        prev
+      else
+        loop(n - 1, cur, prev + cur)
+
     loop(n, 0, 1)
   }
 
